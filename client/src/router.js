7f9f0f20cf95349e3752e6377eec340376db9212
Stephen Lie <stephenlie55@yahoo.com>
2019-06-28 14:37:10 +0700

    Room untuk main

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Rooms from '@/views/Room.vue'
 
 Vue.use(Router)
 
@@ -20,6 +21,16 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/rooms',
+      name: 'rooms',
+      component: Rooms
+    },
+    {
+      path: '/games/:id',
+      name: 'game',
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
-  ]
+]
 })
